85ed3b7b765b0e2bc338c2bf0bd7f4a89f19bf19
ekelemene <ksuxaskrip2007@gmail.com>
2025-01-28 23:10:26 +0300

    Add files via upload

---
+++ prac 19/Books.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prac_19
+{
+    /// <summary>
+    /// класс книги
+    /// </summary>
+    internal class Books : Program
+    {
+        ///1. поля: название, автор, год выпуска, объем в листах.    
+        public string title;
+        public string autor;
+        public int age;
+        public int pages;
+
+        /// <summary>
+        /// метод для вывода 
+        /// </summary>
+        public void Info()
+        {
+            Console.WriteLine($"название: {title}\nавтор: {autor}\nгод выпуска:{age}\nстраницы: {pages}");
+        }
+
+
+
+
+    }
+}
---
+++ prac 19/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prac_19
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Books boo = new Books();
+            ///делаем ввод с клавиатуры
+            Console.Write("название:");
+            boo.title = Console.ReadLine();
+            Console.Write("автор:");
+            boo.autor = Console.ReadLine();
+            Console.Write("год:");
+            boo.age = int.Parse(Console.ReadLine());
+            Console.Write("страницы:");
+            boo.pages = int.Parse(Console.ReadLine());
+            ///используем метод для вывода 
+            boo.Info();
+        }
+    }
+}
